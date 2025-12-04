005e7365f00a2f2ec82ba44bc1de060d3977ef4e
Paul Songin <songinpavel2006@gmail.com>
2025-12-04 15:57:06 +0300

    third lab

---
+++ Main2/Main/Sentence.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace TextTokenizer
+{
+    public enum SentenceType
+    {
+        Declarative,  // .
+        Question,     // ?
+        Exclamation,  // !
+        Unknown
+    }
+
+    public class Sentence
+    {
+        public List<Word> Words { get; set; } = new();
+        public SentenceType Type { get; set; } = SentenceType.Unknown;
+
+        public Sentence() { }
+
+        public static Sentence Parse(string raw)
+        {
+            Sentence s = new();
+            var matches = Regex.Matches(raw, @"\w+|[.!?]");
+            foreach (Match m in matches)
+            {
+                if (Regex.IsMatch(m.Value, @"\w+"))
+                    s.Words.Add(new Word(m.Value));
+                else
+                {
+                    s.Type = m.Value switch
+                    {
+                        "." => SentenceType.Declarative,
+                        "?" => SentenceType.Question,
+                        "!" => SentenceType.Exclamation,
+                        _ => SentenceType.Unknown
+                    };
+                }
+            }
+            return s;
+        }
+
+        public bool IsQuestion => Type == SentenceType.Question;
+        public int WordCount => Words.Count;
+
+        public override string ToString()
+        {
+            string ending = Type switch
+            {
+                SentenceType.Question => "?",
+                SentenceType.Exclamation => "!",
+                SentenceType.Declarative => ".",
+                _ => ""
+            };
+            return string.Join(" ", Words.Select(w => w.Value)) + ending;
+        }
+    }
+}
---
+++ Main2/Main/Word.cs
@@ -0,0 +1,21 @@
+﻿namespace TextTokenizer
+{
+    public class Word
+    {
+        public string Value { get; set; }
+
+        public Word() { }
+        public Word(string value) => Value = value;
+
+        public int Length => Value?.Length ?? 0;
+
+        public bool StartsWithConsonant()
+        {
+            if (string.IsNullOrEmpty(Value)) return false;
+            char first = char.ToLower(Value[0]);
+            return "бвгджзйклмнпрстфхцчшщbcdfghjklmnpqrstvwxyz".Contains(first);
+        }
+
+        public override string ToString() => Value;
+    }
+}
